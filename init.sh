f7dff187929c053b4ca71c1ac442a477852ca3f7
Duc <ducpa@bap.jp>
2023-07-11 14:01:23 +0700

    fix: add zsh

--- init.sh
+++ init.sh
@@ -3,11 +3,17 @@
 dotfiles_dir="$HOME/.dotfiles"
 repository_url=git@github.com:ducpa-bap/dotfiles.git
 
-# Kiểm tra xem thư mục .dotfiles có tồn tại hay không
 if [ -d "$dotfiles_dir" ]; then
-  # Thư mục .dotfiles đã tồn tại, pull code mới nhất từ repository
   git -C "$dotfiles_dir" pull origin
 else
-  # Thư mục .dotfiles chưa tồn tại, clone repository vào thư mục .dotfiles
   git clone "$repository_url" "$dotfiles_dir"
 fi
+
+dotfiles_dir="$HOME/.dotfiles"
+
+for file in "$dotfiles_dir/.*"; do
+  if [ -f "$file" ] && [ "$(basename "$file")" != ".." ]; then
+    echo "Installing $(basename "$file")"
+    ln -sfn "$dotfiles_dir/$(basename "$file")" "$HOME/$(basename "$file")"
+  fi
+done
