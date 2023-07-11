5b8be5d03d6adcac643a55165e8b0c4bfe0ccaea
Duc <ducpa@bap.jp>
2023-07-11 15:51:52 +0700

    install zsh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 repo_dir="$HOME/.dotfiles"
 dotfiles_dir="$repo_dir/dot"
-repository_url=git@github.com:ducpa-bap/dotfiles.git
+repository_url=https://github.com/ducpa-bap/dotfiles.git
 
 if [ -d "$repo_dir" ]; then
   git -C "$repo_dir" pull origin
@@ -16,3 +16,10 @@ for file in "$dotfiles_dir"/*; do
     ln -sfn "$dotfiles_dir/$(basename "$file")" "$HOME/.$(basename "$file")"
   fi
 done
+
+# Install zsh
+if test ! $(which omz); then
+  echo "...Installing ZSH"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "$(curl -s https://raw.githubusercontent.com/zakaziko99/agnosterzak-ohmyzsh-theme/master/agnosterzak.zsh-theme)" > "$ZSH_CUSTOM/themes/agnosterzak.zsh-theme"
+fi
