ca314835900660fb895339a1a8d7b686528dfc65
Duc <ducpa@bap.jp>
2023-07-11 14:39:25 +0700

    moving folder to dot

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-dotfiles_dir="$HOME/.dotfiles"
+repo_dir="$HOME/.dotfiles"
+dotfiles_dir="$repo_dir/dot"
 repository_url=git@github.com:ducpa-bap/dotfiles.git
 
-if [ -d "$dotfiles_dir" ]; then
-  git -C "$dotfiles_dir" pull origin
+if [ -d "$repo_dir" ]; then
+  git -C "$repo_dir" pull origin
 else
-  git clone "$repository_url" "$dotfiles_dir"
+  git clone "$repository_url" "$repo_dir"
 fi
 
-for file in "$dotfiles_dir/dot"/*; do
-  base_file_name = $(basename "$file")
-  if [ -f "$file" ] && [ "$base_file_name" != ".." ]; then
-    echo "Installing $base_file_name"
-    ln -sfn "$dotfiles_dir/$base_file_name" "$HOME/.$base_file_name"
+for file in "$dotfiles_dir"/*; do
+  if [ -f "$file" ] && [ "$(basename "$file")" != ".." ]; then
+    echo "...Installing .$(basename "$file")"
+    ln -sfn "$dotfiles_dir/$(basename "$file")" "$HOME/.$(basename "$file")"
   fi
 done
