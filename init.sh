da3f19a98502b800900f34eccf0d9c4566668520
Duc <ducpa@bap.jp>
2023-11-22 16:39:34 +0700

    fix: update flow

--- init.sh
+++ init.sh
@@ -1,40 +1,25 @@
 #!/bin/bash
 
-repo_dir="$HOME/.dotfiles"
-dotfiles_dir="$repo_dir/dot"
-repository_url=https://github.com/ducpa-bap/dotfiles.git
+DOTFILES_DIR=~/dotfiles
 
-if [ -d "$repo_dir" ]; then
-  git -C "$repo_dir" pull origin
+sudo apt update
+sudo apt install -y git vim tmux
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if [ -d "$DOTFILES_DIR" ]; then
+    cd "$DOTFILES_DIR"
+    git pull origin master
 else
-  git clone "$repository_url" "$repo_dir"
+    git clone https://github.com/ducpa-bap/dotfiles.git "$DOTFILES_DIR"
 fi
 
-for file in "$dotfiles_dir"/*; do
-  if [ -f "$file" ] && [ "$(basename "$file")" != ".." ]; then
-    echo "...Installing .$(basename "$file")"
-    ln -sfn "$dotfiles_dir/$(basename "$file")" "$HOME/.$(basename "$file")"
-  fi
-done
+mv ~/.gitconfig ~/.gitconfig_bk
+# mv ~/.vimrc ~/.vimrc_bk
+# mv ~/.tmux.conf ~/.tmux.conf_bk
+# mv ~/.zshrc ~/.zshrc_bk
 
-save_file() {
-  local file_path="$1"
-  local content="$2"
-
-  # Lấy đường dẫn thư mục chứa từ đường dẫn tệp
-  local dir_path=$(dirname "$file_path")
-
-  # Tạo thư mục chứa nếu chưa tồn tại
-  mkdir -p "$dir_path"
-
-  # Lưu nội dung vào tệp
-  echo "$content" > "$file_path"
-}
-
-# Install zsh
-if test ! $(which omz); then
-  echo "...Installing ZSH"
-  sudo apt install -y zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  save_file "$HOME/.oh-my-zsh/custom/themes/agnosterzak.zsh-theme" "$(curl -s https://raw.githubusercontent.com/zakaziko99/agnosterzak-ohmyzsh-theme/master/agnosterzak.zsh-theme)"
-fi
+ln -sf "$DOTFILES_DIR/dot/.gitconfig" ~/.gitconfig
+# ln -sf "$DOTFILES_DIR/.bashrc" ~/.bashrc
+# ln -sf "$DOTFILES_DIR/.vimrc" ~/.vimrc
+# ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
